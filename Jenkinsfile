80aecd403e3fed2539bce9560d73d27fb080c4ec
allan <aa_g@hotmail.com>
2021-12-14 21:57:01 -0500

    Added coverage configuration

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,16 @@
 pipeline {
     agent { docker { image 'python:3.7' } }
     stages {
-        stage('build') {
+        stage('Build') {
             steps {
                 sh 'python --version'
+                sh 'cd mock_site'
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('Test'){
+            steps{
+                sh 'pytest --cov-config=.coveragerc --cov=.'
             }
         }
     }
--- mock_site/polls/tests.py
+++ mock_site/polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from polls.models import Question
+import datetime
 
-# Create your tests here.
+
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(question_text="Question", pub_date=datetime.datetime.now())
+
+    def test_question_name(self):
+        """Test question name"""
+        question = Question.objects.get(question_text="Question")
+        self.assertEqual(str(question), 'Question')
